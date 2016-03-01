7d1e7584360a02bc4d53a44d4001936a370c0da7
Holger Terhoeven <h.terhoeven@rto.de>
2016-03-01 15:53:31 +0100

    Add CustomShortcode class

---
+++ src/lib/CustomShortcode.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lib;
+
+
+use MBVMedia\Lib\BambeeShortcode;
+
+/**
+ * Created by PhpStorm.
+ * User: hterhoeven
+ * Date: 01.03.2016
+ * Time: 12:02
+ */
+abstract class CustomShortcode extends BambeeShortcode {
+
+}

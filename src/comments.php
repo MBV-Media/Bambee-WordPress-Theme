a7d8c06dff6a576b09713631c930e393f9a8d392
Holger Terhoeven <h.terhoeven@rto.de>
2016-05-27 16:48:23 +0200

    Fix comment pagination

--- src/comments.php
+++ src/comments.php
@@ -4,7 +4,7 @@
     <?php die( __( 'The file "comments.php" can not be accessed directly.', TextDomain ) ); ?>
 <?php endif; ?>
 
-<?php global $bambeeWebsite; ?>
+<?php global $bambee,$bambeeWebsite; ?>
 
     <div class="comment-form">
         <?php comment_form( array(
@@ -19,7 +19,7 @@
     </div>
 
 <?php if ( have_comments() ) : ?>
-    <?php $pagination = $bambeeWebsite->commentPagination(); ?>
+    <?php $pagination = $bambeeWebsite->getCommentPagination(); ?>
     <?php echo $pagination; ?>
     <section class="comments">
         <div class="sideline"></div>
@@ -27,12 +27,11 @@
             <?php
             $comments = get_comments( array(
                 'post_id' => get_the_ID(),
-                'status' => 'approve'
+                'status' => 'approve',
             ) );
             wp_list_comments( array(
                 'style' => 'ul',
                 'avatar_size' => 0,
-                'per_page' => get_option( 'comments_per_page' ),
                 'callback' => array( $bambeeWebsite, 'commentList' ),
             ), $comments );
             ?>

f55a2ecdb93bc04adb38879a8f9cf20f9f18aadb
Holger Terhoeven <h.terhoeven@rto.de>
2017-04-18 15:21:41 +0200

    Remove core- and global-data admin pages

--- lib/bambee-composer/src/MBVMedia/BambeeAdmin.php
+++ lib/bambee-composer/src/MBVMedia/BambeeAdmin.php
@@ -7,8 +7,6 @@
 namespace MBVMedia;
 
 
-use MagicAdminPage\MagicAdminPage;
-use MBVMedia\Shortcode\Lib\ShortcodeManager;
 
 /**
  * The class representing the WordPress Admin.
@@ -18,18 +16,6 @@ use MBVMedia\Shortcode\Lib\ShortcodeManager;
  * @licence MIT
  */
 abstract class BambeeAdmin extends BambeeBase {
-
-    /**
-     * @since 1.4.2
-     * @var MagicAdminPage
-     */
-    private $coreDataPage;
-
-    /**
-     * @since 1.4.2
-     * @var MagicAdminPage
-     */
-    private $globalDataPage;
 
     /**
      * @var integer
@@ -57,133 +43,6 @@ abstract class BambeeAdmin extends BambeeBase {
             add_filter( 'edit_' . $postType . '_per_page', array( $this, 'modifyPostPerPageLimit' ) );
         }
         add_filter('manage_posts_columns' , array( $this, 'customColumns' ) );
-    }
-
-    /**
-     *
-     * @since 1.4.2
-     */
-    public function setupCoreDataPage() {
-
-
-        $componentUrl = Bambee::self()->getComponentUrl();
-
-        $this->coreDataPage = new MagicAdminPage(
-            'core-data',
-            __( 'Core data', TextDomain ),
-            __( 'Core data', TextDomain ),
-            50,
-            $componentUrl . '/img/icons/core-data.png'
-        );
-
-        $this->coreDataPage->addFields( array(
-            'coreDataDescription' => array(
-                'type' => 'description',
-                'title' => 'Shortcodes',
-                'description' => __(
-                    'You can use the [coredata]key[coredata]' .
-                    ' shortcode to display the core data field inside a post.',
-                    TextDomain
-                ),
-            ),
-            'address' => array(
-                'type' => 'textarea',
-                'title' => __( 'Address', TextDomain ),
-            ),
-            'email' => array(
-                'type' => 'textarea',
-                'title' => __( 'E-Mail address', TextDomain ),
-            ),
-            'phone' => array(
-                'type' => 'textarea',
-                'title' => __( 'Phone', TextDomain ),
-            ),
-            'coreGoogleMapsTitle' => array(
-                'type' => 'description',
-                'title' => __( 'Google Maps' ),
-                'description' => '',
-            ),
-            'lat' => array(
-                'type' => 'text',
-                'title' => __( 'Latitude', TextDomain ),
-            ),
-            'lng' => array(
-                'type' => 'text',
-                'title' => __( 'Longitude', TextDomain ),
-            ),
-            'gmapsApiKey' => array(
-                'type' => 'text',
-                'title' => __( 'API-Key', TextDomain ),
-            ),
-            'coreGoogleAnalyticsTitle' => array(
-                'type' => 'description',
-                'title' => __( 'Google Analytics' ),
-                'description' => '',
-            ),
-            'googleTrackingCode' => array(
-                'type' => 'text',
-                'title' => __( 'Tracking-Code', TextDomain ),
-                'default' => 'UA-XXXXX-X',
-            ),
-        ) );
-
-        $this->postPerPageLimit = 50;
-    }
-
-    /**
-     *
-     * @since 1.4.2
-     */
-    private function setupGlobalDataPage() {
-
-        $componentUrl = Bambee::self()->getComponentUrl();
-
-        $this->globalDataPage = new MagicAdminPage(
-            'global-data',
-            __( 'Global data', TextDomain ),
-            __( 'Global data', TextDomain ),
-            51,
-            $componentUrl . '/img/icons/global-data.png'
-        );
-
-        $this->globalDataPage->addField( array(
-            'name' => 'globalDataDescription',
-            'type' => 'label',
-            'title' => 'Shortcodes',
-            'description' => __(
-                'You can use the [globaldata]key[globaldata] ' .
-                ' shortcode to display the global data field inside a post.',
-                TextDomain
-            ),
-        ) );
-    }
-
-    /**
-     * @return MagicAdminPage
-     */
-    public function getCoreDataPage() {
-        return $this->coreDataPage;
-    }
-
-    /**
-     * @param MagicAdminPage $coreDataPage
-     */
-    public function setCoreDataPage( MagicAdminPage $coreDataPage ) {
-        $this->coreDataPage = $coreDataPage;
-    }
-
-    /**
-     * @return MagicAdminPage
-     */
-    public function getGlobalDataPage() {
-        return $this->globalDataPage;
-    }
-
-    /**
-     * @param MagicAdminPage $globalDataPage
-     */
-    public function setGlobalDataPage( MagicAdminPage $globalDataPage ) {
-        $this->globalDataPage = $globalDataPage;
     }
 
     /**

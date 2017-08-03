06a7fa4f0b7debd8426e9c26b240ff0d45e56685
Holger Terhoeven <h.terhoeven@rto.de>
2017-08-03 16:01:06 +0200

    Fix ajax for cookie notice

--- src/partials/cookie-notice.php
+++ src/partials/cookie-notice.php
@@ -1,5 +1,5 @@
 <div class="cookie-notice text-center">
     <?php _e( 'Diese Seite verwendet Cookies. Mit dem Besuch dieser Seite stimmen Sie der Nutzung von Cookies zu.' , TextDomain ); ?>
     <a href="http://www.google.com/intl/de/policies/privacy/partners/" target="_blank"><?php _e(' Mehr Informationen '); ?></a>
-    <a href="?cookie" class="js-hide"><?php _e(' Ok '); ?></a>
+    <a href="?cookie" class="js-hide" data-prevent-default><?php _e(' Ok '); ?></a>
 </div>
